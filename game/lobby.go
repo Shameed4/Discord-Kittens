2eeadba9c3d556417f9a400c1c4e3247557e3a62
Sean Erfan <seanerfan@gmail.com>
2026-07-08 22:11:47 -0400

    feat: added last action to lobby snapshot, added deserializing lobbies

--- game/lobby.go
+++ game/lobby.go
@@ -255,6 +255,7 @@ type LobbySnapshot struct {
 	UnderAttack     bool
 	DiscardPile     []Card
 	ActionLog       []CompletedActionSnapshot
+	LastAction      CompletedActionSnapshot
 
 	TargetedPlayer int // relevant for favor, targetedAttack, 2 and 3 card combos
 
@@ -263,7 +264,7 @@ type LobbySnapshot struct {
 }
 
 func (lobby *Lobby) SerializeLobby() *LobbySnapshot {
-	res := &LobbySnapshot{
+	snapshot := &LobbySnapshot{
 		Name:            lobby.name,
 		Deck:            lobby.deck,
 		PlayersList:     make([]PlayerSnapshot, len(lobby.playersList)),
@@ -277,10 +278,14 @@ func (lobby *Lobby) SerializeLobby() *LobbySnapshot {
 		TargetedPlayer:  lobby.targetedPlayer,
 		NopeDeadline:    lobby.nopeDeadline,
 		ActionLog:       make([]CompletedActionSnapshot, len(lobby.actionLog)),
+		LastAction: CompletedActionSnapshot{
+			Public:  lobby.lastAction.Public,
+			Private: lobby.lastAction.Private,
+		},
 	}
 
 	if lobby.pendingAction != nil {
-		res.PendingAction = &PendingNopeableActionSnapshot{
+		snapshot.PendingAction = &PendingNopeableActionSnapshot{
 			PlayerId:       lobby.pendingAction.playerId,
 			ActionType:     lobby.pendingAction.actionType,
 			PlayedCard:     lobby.pendingAction.playedCard,
@@ -292,7 +297,7 @@ func (lobby *Lobby) SerializeLobby() *LobbySnapshot {
 	}
 
 	for i, p := range lobby.playersList {
-		res.PlayersList[i] = PlayerSnapshot{
+		snapshot.PlayersList[i] = PlayerSnapshot{
 			Hand:          p.Hand,
 			Id:            p.Id,
 			DiscordUserId: p.DiscordUserId,
@@ -303,12 +308,75 @@ func (lobby *Lobby) SerializeLobby() *LobbySnapshot {
 	}
 
 	for i, a := range lobby.actionLog {
-		res.ActionLog[i] = CompletedActionSnapshot{
+		snapshot.ActionLog[i] = CompletedActionSnapshot{
 			Public:  a.Public,
 			Private: a.Private,
 		}
 	}
-	return res
+	return snapshot
+}
+
+func DeserializeLobby(snapshot *LobbySnapshot) *Lobby {
+	lobby := &Lobby{
+		name:            snapshot.Name,
+		deck:            snapshot.Deck,
+		nextId:          snapshot.NextId,
+		currentPlayerId: snapshot.CurrentPlayerId,
+		turnState:       snapshot.TurnState,
+		livingPlayers:   snapshot.LivingPlayers,
+		turnsToTake:     snapshot.TurnsToTake,
+		underAttack:     snapshot.UnderAttack,
+		discardPile:     snapshot.DiscardPile,
+		targetedPlayer:  snapshot.TargetedPlayer,
+		nopeDeadline:    snapshot.NopeDeadline,
+		playersList:     make([]*Player, len(snapshot.PlayersList)),
+		playersMap:      make(map[int]*Player, len(snapshot.PlayersList)),
+		spectators:      make(map[int]*Spectator, 0),
+		actionLog:       make([]CompletedAction, len(snapshot.ActionLog)),
+		lastAction: CompletedAction{
+			Public:  snapshot.LastAction.Public,
+			Private: snapshot.LastAction.Private,
+		},
+		ActionQueue: make(chan PlayerAction),
+		JoinQueue:   make(chan JoinRequest),
+		done:        make(chan struct{}),
+	}
+
+	if snapshot.PendingAction != nil {
+		lobby.pendingAction = &PendingNopeableAction{
+			playerId:       snapshot.PendingAction.PlayerId,
+			actionType:     snapshot.PendingAction.ActionType,
+			playedCard:     snapshot.PendingAction.PlayedCard,
+			comboSize:      snapshot.PendingAction.ComboSize,
+			targetedPlayer: snapshot.PendingAction.TargetedPlayer,
+			requestedCard:  snapshot.PendingAction.RequestedCard,
+			isNoped:        snapshot.PendingAction.IsNoped,
+		}
+		lobby.nopeTimer = time.NewTimer(time.Until(snapshot.NopeDeadline))
+	}
+
+	for i, p := range snapshot.PlayersList {
+		newPlayer := &Player{
+			Hand:          p.Hand,
+			Id:            p.Id,
+			DiscordUserId: p.DiscordUserId,
+			Name:          p.Name,
+			Avatar:        p.Avatar,
+			IsAlive:       p.IsAlive,
+			IsOnline:      false,
+		}
+		lobby.playersList[i] = newPlayer
+		lobby.playersMap[p.Id] = newPlayer
+	}
+
+	for i, a := range snapshot.ActionLog {
+		lobby.actionLog[i] = CompletedAction{
+			Public:  a.Public,
+			Private: a.Private,
+		}
+	}
+
+	return lobby
 }
 
 func NewLobby(name string) *Lobby {
